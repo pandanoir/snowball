3a32003f8c2f406bda99d1055d97fcaa8ef446ba
PandaNoir <moon_in_france@yahoo.co.jp>
2013-01-03 17:41:16 +0900

    QUnitのいろいろを圧縮
    よくわからないものも全部あっしゅくした

--- qunit/addons/canvas/qunit-canvas.js
+++ qunit/addons/canvas/qunit-canvas.js
@@ -1,6 +1,1 @@
-QUnit.extend( QUnit, {
-	pixelEqual: function(canvas, x, y, r, g, b, a, message) {
-		var actual = Array.prototype.slice.apply(canvas.getContext('2d').getImageData(x, y, 1, 1).data), expected = [r, g, b, a];
-		QUnit.push(QUnit.equiv(actual, expected), actual, expected, message);
-	}
-});
+QUnit.extend(QUnit,{pixelEqual:function(a,c,d,b,e,f,g,h){a=Array.prototype.slice.apply(a.getContext("2d").getImageData(c,d,1,1).data);b=[b,e,f,g];QUnit.push(QUnit.equiv(a,b),a,b,h)}});
--- qunit/addons/step/step-test.js
+++ qunit/addons/step/step-test.js
@@ -1,13 +1,1 @@
-module('Step Addon');
-test("step", 3, function () {
-	QUnit.step(1, "step starts at 1");
-	setTimeout(function () {
-		start();
-		QUnit.step(3);
-	}, 100);
-	QUnit.step(2, "before the setTimeout callback is run");
-	stop();
-});
-test("step counter", 1, function () {
-	QUnit.step(1, "each test has its own step counter");
-});
+(function(a){module("Step Addon");test("step",3,function(){a.step(1,"step starts at 1");setTimeout(function(){start();a.step(3)},100);a.step(2,"before the setTimeout callback is run");stop()});test("step counter",1,function(){a.step(1,"each test has its own step counter")})})(QUnit);
