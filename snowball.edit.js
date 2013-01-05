1b3129c1677fb362f28e3ca03e1267ade7458ad0
PandaNoir <moon_in_france@yahoo.co.jp>
2013-01-06 00:43:43 +0900

    void 0の削除、colorの追記
    #ff0000を考えてなかった。あと、void 0意味ないと思って消した。

--- snowball.edit.js
+++ snowball.edit.js
@@ -81,7 +81,6 @@
 						$compress.trigger("click")
 					},this)
 				}
-				files=event=void 0;
 			})
 		}else{
 			$bef.attr("placeholder","ここにコードをペーストしてください。どうやらお使いのブラウザではドラッグアンドドロップは対応していないようです。");
@@ -172,7 +171,6 @@
 			function cb(g) {
 				//バイト数カウント
 				for(var c = d = 0, b = g.length; d < b; d++) c += 4 > escape(g.charAt(d)).length ? 1 : 2;
-				b=d=void 0;
 				return c
 			}
 			/*		function g(a) {
@@ -222,7 +220,7 @@
 						paddings = properties.match(pattern);
 
 					if(paddings != null) {
-						var paddingsData={top:null,bottom:null,right:null,left:null};
+						var paddingsData={t:null,b:null,r:null,l:null};
 						for(var i = 0, j = paddings.length; i < j; i++) {
 							paddings[i] = paddings[i].replace(pattern, "$1:$2").split(":");
 							paddings[i][1] = paddings[i][1].replace(/;$/, "");
@@ -259,15 +257,12 @@
 								}
 							})
 							block[bi] = block[bi].replace(before, properties);
-							properties=i=j=void 0;
 						}
-						paddingsData=void 0;
 					}
 				}
 				for(var i = 0; i < bj; i++) {
 					if(beforeblock[i] != block[i]) str = str.replace(beforeblock[i], block[i])
 				}
-				blockpattern=pattern=beforeblock=block=before=properties=paddings=property=bi=bj=void 0;
 				return str.replace(RegExp("(" + pm + "[\s]?:[\s]?)([^;$]+?[;$])", "gi"), function (a, property, b) {
 					var values = b.replace(/ /g, ",").split(","),
 						d = values.length,
@@ -289,7 +284,6 @@
 						}
 					}
 					property=property + result
-					d=values=result=void 0;
 					return property
 				})
 			}
@@ -307,7 +301,6 @@
 			$.each(InputPartOption,function(Chara,replaces){
 				if(InputOption[Chara]) b=b.replace(replaces[0],replaces[1]);
 			});
-			InputPartOption=void 0;
 			if(!InputOption.option) {
 				//一部圧縮の際のオプション
 				b = b.replace(/^[\s]*/gm, "")
@@ -330,23 +323,15 @@
 				$.each(InputPartOption,function(Chara,replaces){
 					if(InputOption[Chara]) b=b.replace(replaces[0],replaces[1]);
 				})
-				InputPartOption=void 0;
 			}
-			/*	if (InputOption.font && (block = b.match(/([\#\w\.\, \:\[\]\"\'=-\_]+?)\{([^\{\}]*?)\}/g))) {
-				h = 0;
-				for (e = block.length; h < e; h++) if (selector = block[h].match(/([\#\w\.\, \:\[\]\"\'=-\_]+?)\{([^\{\}]*?)\}/)[1], pro = block[h].match(/([\#\w\.\, \:\[\]\"\'=-\_]+?)\{([^\{\}]*?)\}/)[2], truepro = "", font = [], (fonts = pro.match(/(font-size):(.+?)[\r\n;\}]/gi)) && (fonts = pro.match(/(font-family):(.+?)[\r\n;\}]/gi))) pro = g("(font-style|font-variant|font-weight)"), pro = g("(font-size)"), pro = g("(line-height)"), pro = g("(font-family)"), 0 != font.length && (pro = "font:" + font.join(" ") + ";\n" + pro), truepro = selector + "{" + pro + "}", re = RegExp(block[h]), b = b.replace(re, truepro)
-			}*/
 			if(InputOption.color_name) {
 				//カラーネームの最適化
-				var color = {aliceblue:"#f0f8ff",antiquewhite:"#faebd7",aquamarine:"#7fffd4",black:"#000",blanchedalmond:"#ffebcd",blueviolet:"#8a2be2",burlywood:"#deb887",cadetblue:"#5f9ea0",chartreuse:"#7fff00",chocolate:"#d2691e",cornflowerblue:"#6495ed",cornsilk:"#fff8dc",darkblue:"#00008b",darkcyan:"#008b8b",darkgoldenrod:"#b8860b",darkgray:"#a9a9a9",darkgreen:"#006400",darkkhaki:"#bdb76b",darkmagenta:"#8b008b",darkolivegreen:"#556b2f",darkorange:"#ff8c00",darkorchid:"#9932cc",darksalmon:"#e9967a",darkseagreen:"#8fbc8f",darkslateblue:"#483d8b",darkslategray:"#2f4f4f",darkturquoise:"#00ced1",darkviolet:"#9400d3",deeppink:"#ff1493",deepskyblue:"#00bfff",dodgerblue:"#1e90ff",firebrick:"#b22222",floralwhite:"#fffaf0",forestgreen:"#228b22",fuchsia:"#f0f",gainsboro:"#dcdcdc",ghostwhite:"#f8f8ff",goldenrod:"#daa520",greenyellow:"#adff2f",honeydew:"#f0fff0",indianred:"#cd5c5c",lavendar:"#e6e6fa",lavenderblush:"#fff0f5",lawngreen:"#7cfc00",lemonchiffon:"#fffacd",lightblue:"#add8e6",lightcoral:"#f08080",lightcyan:"#e0ffff",lightgoldenrodyellow:"#fafad2",lightgray:"#d3d3d3",lightgreen:"#90ee90",lightpink:"#ffb6c1",lightsalmon:"#ffa07a",lightseagreen:"#20b2aa",lightskyblue:"#87cefa",lightslategray:"#789",lightsteelblue:"#b0c4de",lightyellow:"#ffffe0",limegreen:"#32cd32",magenta:"#f0f",mediumaquamarine:"#66cdaa",mediumblue:"#0000cd",mediumorchid:"#ba55d3",mediumpurple:"#9370db",mediumseagreen:"#3cb371",mediumslateblue:"#7b68ee",mediumspringgreen:"#00fa9a",mediumturquoise:"#48d1cc",mediumvioletred:"#c71585",midnightblue:"#191970",mintcream:"#f5fffa",mistyrose:"#ffe4e1",moccasin:"#ffe4b5",navajowhite:"#ffdead",olivedrab:"#6b8e23",orangered:"#ff4500",palegoldenrod:"#eee8aa",palegreen:"#98fb98",paleturquoise:"#afeeee",palevioletred:"#db7093",papayawhite:"#ffefd5",peachpuff:"#ffdab9",powderblue:"#b0e0e6",rosybrown:"#bc8f8f",royalblue:"#4169e1",saddlebrown:"#8b4513",sandybrown:"#f4a460",seagreen:"#2e8b57",seashell:"#fff5ee",slateblue:"#6a5acd",slategray:"#708090",springgreen:"#00ff7f",steelblue:"#4682b4",turquoise:"#40e0d0",white:"#fff",whitesmoke:"#f5f5f5",yellow:"#ff0",yellowgreen:"#9acd32","#f0ffff":"azure","#f5f5dc":"beige","#ffe4c4":"bisque","#a52a2a":"brown","#ff7f50":"coral","#ffd700":"gold","#808080":"gray","#008000":"green","#4b0082":"indigo","#fffff0":"ivory","#f0e68c":"khaki","#faf0e6":"linen","#800000":"maroon","#000080":"navy","#808000":"olive","#ffa500":"orange","#da70d6":"orchid","#cd853f":"peru","#ffc0cb":"pink","#dda0dd":"plum","#800080":"purple","#f00":"red","#fa8072":"salmon","#a0522d":"sienna","#c0c0c0":"silver","#fffafa":"snow","#d2b48c":"tan","#008080":"teal","#ff6347":"tomato","#ee82ee":"violet","#f5deb3":"wheat"
-				}, h;
+				var color = {aliceblue:"#f0f8ff",antiquewhite:"#faebd7",aquamarine:"#7fffd4",black:"#000",blanchedalmond:"#ffebcd",blueviolet:"#8a2be2",burlywood:"#deb887",cadetblue:"#5f9ea0",chartreuse:"#7fff00",chocolate:"#d2691e",cornflowerblue:"#6495ed",cornsilk:"#fff8dc",darkblue:"#00008b",darkcyan:"#008b8b",darkgoldenrod:"#b8860b",darkgray:"#a9a9a9",darkgreen:"#006400",darkkhaki:"#bdb76b",darkmagenta:"#8b008b",darkolivegreen:"#556b2f",darkorange:"#ff8c00",darkorchid:"#9932cc",darksalmon:"#e9967a",darkseagreen:"#8fbc8f",darkslateblue:"#483d8b",darkslategray:"#2f4f4f",darkturquoise:"#00ced1",darkviolet:"#9400d3",deeppink:"#ff1493",deepskyblue:"#00bfff",dodgerblue:"#1e90ff",firebrick:"#b22222",floralwhite:"#fffaf0",forestgreen:"#228b22",fuchsia:"#f0f",gainsboro:"#dcdcdc",ghostwhite:"#f8f8ff",goldenrod:"#daa520",greenyellow:"#adff2f",honeydew:"#f0fff0",indianred:"#cd5c5c",lavendar:"#e6e6fa",lavenderblush:"#fff0f5",lawngreen:"#7cfc00",lemonchiffon:"#fffacd",lightblue:"#add8e6",lightcoral:"#f08080",lightcyan:"#e0ffff",lightgoldenrodyellow:"#fafad2",lightgray:"#d3d3d3",lightgreen:"#90ee90",lightpink:"#ffb6c1",lightsalmon:"#ffa07a",lightseagreen:"#20b2aa",lightskyblue:"#87cefa",lightslategray:"#789",lightsteelblue:"#b0c4de",lightyellow:"#ffffe0",limegreen:"#32cd32",magenta:"#f0f",mediumaquamarine:"#66cdaa",mediumblue:"#0000cd",mediumorchid:"#ba55d3",mediumpurple:"#9370db",mediumseagreen:"#3cb371",mediumslateblue:"#7b68ee",mediumspringgreen:"#00fa9a",mediumturquoise:"#48d1cc",mediumvioletred:"#c71585",midnightblue:"#191970",mintcream:"#f5fffa",mistyrose:"#ffe4e1",moccasin:"#ffe4b5",navajowhite:"#ffdead",olivedrab:"#6b8e23",orangered:"#ff4500",palegoldenrod:"#eee8aa",palegreen:"#98fb98",paleturquoise:"#afeeee",palevioletred:"#db7093",papayawhite:"#ffefd5",peachpuff:"#ffdab9",powderblue:"#b0e0e6",rosybrown:"#bc8f8f",royalblue:"#4169e1",saddlebrown:"#8b4513",sandybrown:"#f4a460",seagreen:"#2e8b57",seashell:"#fff5ee",slateblue:"#6a5acd",slategray:"#708090",springgreen:"#00ff7f",steelblue:"#4682b4",turquoise:"#40e0d0",white:"#fff",whitesmoke:"#f5f5f5",yellow:"#ff0",yellowgreen:"#9acd32","#f0ffff":"azure","#f5f5dc":"beige","#ffe4c4":"bisque","#a52a2a":"brown","#ff7f50":"coral","#ffd700":"gold","#808080":"gray","#008000":"green","#4b0082":"indigo","#fffff0":"ivory","#f0e68c":"khaki","#faf0e6":"linen","#800000":"maroon","#000080":"navy","#808000":"olive","#ffa500":"orange","#da70d6":"orchid","#cd853f":"peru","#ffc0cb":"pink","#dda0dd":"plum","#800080":"purple","#ff0000":"red","#f00":"red","#fa8072":"salmon","#a0522d":"sienna","#c0c0c0":"silver","#fffafa":"snow","#d2b48c":"tan","#008080":"teal","#ff6347":"tomato","#ee82ee":"violet","#f5deb3":"wheat"}, h;
 				for(h in color) {
 					if(-1 != b.indexOf(h)){
 						b = b.replace(RegExp("([: ,\)\(]|[\t ]?:[\t ]?)([^;\{\}]*?)" + h + "((?:!important)|[, )(;}\n\r])", "gim"), "$1$2" + color[h] + "$3")
 					}
-				
 				}
-				color=void 0;
 			}
 			if(-1 != b.indexOf("padding") && InputOption.padding) b = makeshort(b, "padding");//パディング最適化
 			if(-1 != b.indexOf("margin") && InputOption.margin) b = makeshort(b, "margin");//マージン最適化
@@ -363,7 +348,6 @@
 						b=b.replace(beforeblock[i],block[i])
 					}
 				}
-				block_pattern=beforeblock=block=void 0;
 			}
 			var afterSize = cb(b);
 			$aft.val(b).select();
@@ -376,7 +360,6 @@
 					.end().find("#aftB").text(m.floor(afterSize / 1024) + " KB(" + afterSize + " B)")
 					.end().find("#minus").text(beforeSize == afterSize ? "±0B(0%)" : "-" + m.floor((beforeSize - afterSize) / 1024) + " KB(" + (beforeSize - afterSize) + " B)("+(100-afterSize*100/beforeSize).toFixed(2)+"%)");
 			}
-			i=j=b=beforeSize=afterSize=m=void 0;
 			$lform.find("#mes").stop(true,false).addClass("on").css("opacity","1").fadeTo(1500, 0.3,function(){$(this).removeClass("on")})
 		}
 	});
