82241f9e1a02ad4d564e9ef3500c7bd10130e298
PandaNoir <moon_in_france@yahoo.co.jp>
2013-01-12 22:53:01 +0900

    CSSBlockの実装をした
    CSSBlockを作った。
    機能は、CSSをブロックで分けて、ブロックのプロパティとセレクタでわけるようにした。いまのところセレクタが意味ない…

--- snowball.edit.js
+++ snowball.edit.js
@@ -175,6 +175,59 @@
 			$results.toggle();
 		});
 		function compress(event) {
+			var b = $bef.val(),
+				beforeSize = cb(b),
+				m = Math;
+			var CSSBlock=function(){
+				var that={};
+				var blocks=[];
+				that.length=0;
+				function push(block){
+					blocks[blocks.length]=block;
+					that.length+=1;
+				};
+				that.push=push
+				that.select=function(num){
+					if(typeof num==="number"){
+						var result={};
+						for(var key in blocks[num])
+							result[key]=blocks[num][key]
+					}else{
+						var result=[]
+						for(var i=0,j=blocks.length;i<j;i++){
+							result[i]={};
+							for(var key in blocks[i]){
+								result[i][key]=blocks[i][key];
+							}
+						}
+					}
+					return result;
+				}
+				that.init=function(str){
+					if(-1!=str.indexOf("{")){
+						var block=str.match(/([.#a-zA-Z ,:\-\[\]=\"\']+?)\{([\s\S]*?)\}/gim);
+						for(var i=0,j=block.length;i<j;i+=1){
+							str.replace(block[i],"");
+							block[i]={
+								selector:/([^\{]+?)\{/.exec(block[i])[1],
+								properties:/\{([\s\S]+?)\}/.exec(block[i])[1],
+								string:block[i],
+								change:function(str){
+									this.string=str;
+									this.selector=/\{([\s\S]+?)\}/.exec(str)[1];
+									this.properties=/\{([\s\S]+?)\}/.exec(str)[1];
+								}
+							}
+							push(block[i]);
+						}
+					}else{
+						push(str)
+					}
+				}
+				return that
+			}
+			CSSBlock=CSSBlock()
+			CSSBlock.init(b);
 			event.pd();
 			event.sp();
 			if(!$befB){var $befB=$results.find("#befB")}
@@ -220,18 +273,17 @@
 				return false
 			};
 			*/
+			//makeshort開始
 			function makeshort(str, pm) {
-				var block_pattern = /([.#\w ,:\-\[\]=\"\']+?)\{([\s\S]*?)\}/gim,
-					block = str.match(block_pattern),
-					beforeblock = str.match(block_pattern),
-					bj = block !== null ? block.length : 0,
-					bi = 0,
+				CSSBlock.init(str)
+				var block=CSSBlock.select(),
+					beforeblock=CSSBlock.select(),
 					pattern = new RegExp("(" + pm + "(?:\-left|\-right|\-top|\-bottom)? ?: ?([^;$]+)([;$]?))", "gim");
-				for(; bi < bj; bi++) {
-					var before = block[bi].match(/([.#\w ,:\-\[\]=\"\']+?)\{([\s\S]*?)\}/im)[2],
-						properties = block[bi].match(/([.#\w ,:\-\[\]=\"\']+?)\{([\s\S]*?)\}/im)[2],
-						paddings = properties.match(pattern);
-
+				for(var bi=0,bj=CSSBlock.length; bi < bj; bi++) {
+					var before = CSSBlock.select(bi),
+						after = CSSBlock.select(bi),
+						paddings = after.properties.match(pattern);
+			
 					if(paddings !== null) {
 						var paddingsData={t:null,b:null,r:null,l:null};
 						for(var i = 0, j = paddings.length; i < j; i++) {
@@ -259,22 +311,22 @@
 								: paddingsData.r === paddingsData.l && paddingsData.t !== paddingsData.b ?
 									paddingsData.t + " " + paddingsData.r + " " + paddingsData.b
 								: paddingsData.t + " " + paddingsData.r + " " + paddingsData.b + " " + paddingsData.l,
-								j = properties.match(pattern),
-								j = j !== null ? j.length : 0,
-								i = 0;
-							properties = properties.replace(RegExp("("+pm+"(?:\-left|\-right|\-top|\-bottom)? ?: ?([^;$]+)([;$]?)([\r\n]?))","gim"),function (a,b,cc,d,endl){
-								if(++i === j) {
+								jjj = after.properties.match(pattern),
+								jjj = jjj !== null ? jjj.length : 0,
+								iii = 0;
+							after.properties = after.properties.replace(RegExp("("+pm+"(?:\-left|\-right|\-top|\-bottom)? ?: ?([^;$]+)([;$]?)([\r\n]?))","gim"),function (a,b,cc,d,endl){
+								if(++iii === jjj) {
 									return pm + ":" + c + (d||"")+endl;
 								} else {
 									return ""
 								}
 							})
-							block[bi] = block[bi].replace(before, properties);
+							block[bi].change(block[bi].string.replace(before.properties, after.properties));
 						}
 					}
 				}
 				for(var i = 0; i < bj; i++) {
-					if(beforeblock[i] !== block[i]) str = str.replace(beforeblock[i], block[i])
+					if(beforeblock[i].string !== block[i].string) str = str.replace(beforeblock[i].string, block[i].string)
 				}
 				return str.replace(RegExp("(" + pm + "[\s]?:[\s]?)([^;$]+?[;$])", "gi"), function (a, property, b) {
 					var values = b.replace(/ /g, ",").split(","),
@@ -300,9 +352,8 @@
 					return property
 				})
 			}
-			var b = $bef.val(),
-				beforeSize = cb(b),
-				m = Math;
+			
+			//makeshort終わり
 			for(var i=0,j=$input.length;i<j;i++){
 				InputOption[$input.eq(i).attr("id")]=$input.eq(i).attr("checked");
 			}
@@ -340,25 +391,34 @@
 			if(InputOption.color_name) {
 				//カラーネームの最適化
 				var color = {aliceblue:"#f0f8ff",antiquewhite:"#faebd7",aquamarine:"#7fffd4",black:"#000",blanchedalmond:"#ffebcd",blueviolet:"#8a2be2",burlywood:"#deb887",cadetblue:"#5f9ea0",chartreuse:"#7fff00",chocolate:"#d2691e",cornflowerblue:"#6495ed",cornsilk:"#fff8dc",darkblue:"#00008b",darkcyan:"#008b8b",darkgoldenrod:"#b8860b",darkgray:"#a9a9a9",darkgreen:"#006400",darkkhaki:"#bdb76b",darkmagenta:"#8b008b",darkolivegreen:"#556b2f",darkorange:"#ff8c00",darkorchid:"#9932cc",darksalmon:"#e9967a",darkseagreen:"#8fbc8f",darkslateblue:"#483d8b",darkslategray:"#2f4f4f",darkturquoise:"#00ced1",darkviolet:"#9400d3",deeppink:"#ff1493",deepskyblue:"#00bfff",dodgerblue:"#1e90ff",firebrick:"#b22222",floralwhite:"#fffaf0",forestgreen:"#228b22",fuchsia:"#f0f",gainsboro:"#dcdcdc",ghostwhite:"#f8f8ff",goldenrod:"#daa520",greenyellow:"#adff2f",honeydew:"#f0fff0",indianred:"#cd5c5c",lavendar:"#e6e6fa",lavenderblush:"#fff0f5",lawngreen:"#7cfc00",lemonchiffon:"#fffacd",lightblue:"#add8e6",lightcoral:"#f08080",lightcyan:"#e0ffff",lightgoldenrodyellow:"#fafad2",lightgray:"#d3d3d3",lightgreen:"#90ee90",lightpink:"#ffb6c1",lightsalmon:"#ffa07a",lightseagreen:"#20b2aa",lightskyblue:"#87cefa",lightslategray:"#789",lightsteelblue:"#b0c4de",lightyellow:"#ffffe0",limegreen:"#32cd32",magenta:"#f0f",mediumaquamarine:"#66cdaa",mediumblue:"#0000cd",mediumorchid:"#ba55d3",mediumpurple:"#9370db",mediumseagreen:"#3cb371",mediumslateblue:"#7b68ee",mediumspringgreen:"#00fa9a",mediumturquoise:"#48d1cc",mediumvioletred:"#c71585",midnightblue:"#191970",mintcream:"#f5fffa",mistyrose:"#ffe4e1",moccasin:"#ffe4b5",navajowhite:"#ffdead",olivedrab:"#6b8e23",orangered:"#ff4500",palegoldenrod:"#eee8aa",palegreen:"#98fb98",paleturquoise:"#afeeee",palevioletred:"#db7093",papayawhite:"#ffefd5",peachpuff:"#ffdab9",powderblue:"#b0e0e6",rosybrown:"#bc8f8f",royalblue:"#4169e1",saddlebrown:"#8b4513",sandybrown:"#f4a460",seagreen:"#2e8b57",seashell:"#fff5ee",slateblue:"#6a5acd",slategray:"#708090",springgreen:"#00ff7f",steelblue:"#4682b4",turquoise:"#40e0d0",white:"#fff",whitesmoke:"#f5f5f5",yellow:"#ff0",yellowgreen:"#9acd32","#f0ffff":"azure","#f5f5dc":"beige","#ffe4c4":"bisque","#a52a2a":"brown","#ff7f50":"coral","#ffd700":"gold","#808080":"gray","#008000":"green","#4b0082":"indigo","#fffff0":"ivory","#f0e68c":"khaki","#faf0e6":"linen","#800000":"maroon","#000080":"navy","#808000":"olive","#ffa500":"orange","#da70d6":"orchid","#cd853f":"peru","#ffc0cb":"pink","#dda0dd":"plum","#800080":"purple","#ff0000":"red","#f00":"red","#fa8072":"salmon","#a0522d":"sienna","#c0c0c0":"silver","#fffafa":"snow","#d2b48c":"tan","#008080":"teal","#ff6347":"tomato","#ee82ee":"violet","#f5deb3":"wheat"}, h;
+				CSSBlock.init(b);
 				for(h in color) {
-					if(-1 !== b.indexOf(h)){
-						b = b.replace(RegExp("([: ,\)\(]|[\t ]?:[\t ]?)([^;\{\}]*?)" + h + "((?:!important)|[, )(;}\n\r])", "gim"), "$1$2" + color[h] + "$3")
+					for(var i=0,j=CSSBlock.length;i<j;i++){
+						if(-1 !== CSSBlock.select(i).properties.indexOf(h)){
+							b=b.replace(CSSBlock.select(i).properties,CSSBlock.select(i).properties.replace(RegExp("([: ,\)\(]|[\t ]?:[\t ]?)([^;\{\}]*?)" + h + "((?:!important)|[, )(;}\n\r])", "gim"), "$1$2" + color[h] + "$3"))
+						}
 					}
 				}
 			}
 			if(-1 !== b.indexOf("padding") && InputOption.padding) b = makeshort(b, "padding");//パディング最適化
 			if(-1 !== b.indexOf("margin") && InputOption.margin) b = makeshort(b, "margin");//マージン最適化
-			if(InputOption.color) b = b.replace(/\#([0-9a-fA-F])\1([0-9a-fA-F])\2([0-9a-fA-F])\3/g, "#$1$2$3");//カラーを6桁から3桁へ
+			if(InputOption.color){
+				CSSBlock.init(b)
+				var block=CSSBlock.select()
+				for(var i=0,j=CSSBlock.length;i<j;i++){
+					b = b.replace(CSSBlock.select(i).properties,CSSBlock.select(i).properties.replace(/\#([0-9a-fA-F])\1([0-9a-fA-F])\2([0-9a-fA-F])\3/g, "#$1$2$3"));//カラーを6桁から3桁へ
+				}
+			}
 			if(InputOption.lower){
-				var block_pattern = /([.#\w ,:\-\[\]=\"\']+?)\{([\s\S]*?)\}/gim,
-					block = b.match(block_pattern),
-					beforeblock = b.match(block_pattern);
-				if(block !== null ){
+				CSSBlock.init(b)
+				var block = CSSBlock.select(),
+					beforeblock = CSSBlock.select();
+				if(block.properties !== "" ){
 					for(var i=0,j=block.length;i<j;i++){
-						block[i] = block[i].replace(/([: ,)(]|[\t ]?:[\t ]?)(#[0-9a-fA-F][0-9a-fA-F]?[0-9a-fA-F][0-9a-fA-F]?[0-9a-fA-F][0-9a-fA-F]?)/g, function(all,prop,one){
+						block[i].properties = block[i].properties.replace(/([: ,)(]|[\t ]?:[\t ]?)(#[0-9a-fA-F][0-9a-fA-F]?[0-9a-fA-F][0-9a-fA-F]?[0-9a-fA-F][0-9a-fA-F]?)/g, function(all,prop,one){
 							return prop+(one.toLowerCase())
 						});//カラーを小文字か大文字に
-						b=b.replace(beforeblock[i],block[i])
+						b=b.replace(beforeblock[i].properties,block[i].properties)
 					}
 				}
 			}
